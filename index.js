53b317b54b91055973e341aba45dcfb0c2a2af3a
jerodepr <depraetere.jeroen1@gmail.com>
2017-05-15 13:28:22 +0200

    proberen blog online te zetten

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 var express = require("express");
 var path = require("path");
+request = require('request-json');
 var app = express();
+const instagramPosts = require('instagram-posts');
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'))
+
+
 app.get('/', function(req, res) {
-  res.render("index", {
-    naam: "Wereld"
+  instagramPosts('depraeterejeroen').then(afbeeldingen => {
+      res.render("index", {
+        afbeeldingen: afbeeldingen
+      });
   });
 });
+
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
--- public/js/template.js
+++ public/js/template.js
@@ -1,5 +1,4 @@
 function main() {
-          // hier komt je code
 }
 
 window.onload = function() {
